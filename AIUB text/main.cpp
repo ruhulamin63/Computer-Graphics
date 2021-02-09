c14e8760059b2f4a5840b642a8438ea10da23170
Ruhul Amin <41449686+ruhulamin63@users.noreply.github.com>
2021-02-10 01:28:07 +0600

    Rainbow + AIUB text project file

---
+++ AIUB text/main.cpp
@@ -0,0 +1,153 @@
+#include <windows.h>
+#include <GL/glut.h>
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+    glPointSize(3.0);
+    glLineWidth(3.0);
+//==========================================
+	glBegin(GL_POINTS);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.8f, 0.0f);
+	glVertex2f(-0.7f, 0.0f); //x,y
+
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.8f, 0.0f);
+	glVertex2f(-0.7f, 0.0f);
+//==========================================
+
+    glBegin(GL_POINTS);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.75f, -0.1f);
+	glVertex2f(-0.78f, -0.2f); //x,y
+	glVertex2f(-0.72f, -0.2f);
+
+    glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.75f, -0.1f);
+	glVertex2f(-0.78f, -0.2f); //x,y
+	glVertex2f(-0.72f, -0.2f);
+//==========================================
+    glBegin(GL_POINTS);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.95f, -0.4f);
+	glVertex2f(-0.85f, -0.4f); //x,y
+
+    glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.8f, 0.0f);
+	glVertex2f(-0.95f, -0.4f); //x,y
+	glVertex2f(-0.95f, -0.4f);
+	glVertex2f(-0.85f, -0.4f);
+//==========================================
+    glBegin(GL_POINTS);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.83f, -0.3f);
+	glVertex2f(-0.68f, -0.3f);
+
+    glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.85f, -0.4f);
+	glVertex2f(-0.83f, -0.3f);
+	glVertex2f(-0.83f, -0.3f);
+	glVertex2f(-0.68f, -0.3f);
+//==========================================
+    glBegin(GL_POINTS);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.55f, -0.4f);
+	glVertex2f(-0.65f, -0.4f);
+
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.7f, 0.0f);
+	glVertex2f(-0.55f, -0.4f);
+	glVertex2f(-0.55f, -0.4f);
+	glVertex2f(-0.65f, -0.4f);
+	glVertex2f(-0.65f, -0.4f);
+	glVertex2f(-0.68f, -0.3f);
+
+//**********************************  AAAAAAAA *********************************
+
+//==========================================
+    glBegin(GL_POINTS);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex2f(-0.40f, 0.0f);
+	glVertex2f(-0.30f, 0.0f);
+	glVertex2f(-0.40f, -0.4f);
+	glVertex2f(-0.30f, -0.4f);
+
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.40f, 0.0f);
+	glVertex2f(-0.30f, 0.0f);
+	glVertex2f(-0.40f, -0.4f);
+	glVertex2f(-0.30f, -0.4f);
+
+	glVertex2f(-0.40f, 0.0f);
+	glVertex2f(-0.40f, -0.4f);
+	glVertex2f(-0.30f, 0.0f);
+	glVertex2f(-0.30f, -0.4f);
+
+//******************************** IIIIIII **********************************************
+
+//==========================================
+    glBegin(GL_POINTS);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex2f(-0.15f, 0.0f);
+	glVertex2f(-0.05f, 0.0f);
+	glVertex2f(-0.15f, -0.4f);
+	glVertex2f(-0.05f, -0.3f);
+
+	glVertex2f(0.05f, 0.0f);
+	glVertex2f(0.15f, 0.0f);
+	glVertex2f(0.15f, -0.4f);
+	glVertex2f(0.05f, -0.3f);
+
+	glBegin(GL_LINES);
+	glColor3f(1.0f, 0.0f, 0.0f);
+
+	glVertex2f(-0.15f, 0.0f);
+	glVertex2f(-0.05f, 0.0f);
+
+	glVertex2f(0.05f, 0.0f);
+	glVertex2f(0.15f, 0.0f);
+
+	glVertex2f(-0.05f, 0.0f);
+	glVertex2f(-0.05f, -0.3f);
+
+	glVertex2f(0.05f, 0.0f);
+	glVertex2f(0.05f, -0.3f);
+
+	glVertex2f(-0.05f, -0.3f);
+	glVertex2f(0.05f, -0.3f);
+
+	glVertex2f(-0.15f, 0.0f);
+	glVertex2f(-0.15f, -0.4f);
+
+	glVertex2f(0.15f, 0.0f);
+	glVertex2f(0.15f, -0.4f);
+
+	glVertex2f(-0.15f, -0.4f);
+	glVertex2f(0.15f, -0.4f);
+//*********************************** UUUUUUUUU *****************************************
+
+//==========================================
+//==========================================
+//==========================================
+//==========================================
+    glEnd();
+
+	glFlush();  // Render now
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutCreateWindow("Rainbow Flag");
+	glutInitWindowSize(320, 320);
+	glutDisplayFunc(display);
+	glutMainLoop();
+
+	return 0;
+}
--- Rainbow_flag/main.cpp
+++ Rainbow_flag/main.cpp
@@ -10,50 +10,58 @@ void display() {
     glVertex2f(0.2f, 0.5f);
     glVertex2f(0.2f, 0.4f);
     glVertex2f(-0.6f, 0.4f);
-
+//================================================
     glColor3f(0.0f, 1.0f, 0.0f);
     glVertex2f(-0.6f, 0.4f);
     glVertex2f(0.2f, 0.4f);
     glVertex2f(0.2f,  0.3f);
     glVertex2f(-0.6f,  0.3f);
-
+//================================================
     glColor3f(0.0f, .50f, 0.0f);
     glVertex2f(-0.6f, 0.3f);
     glVertex2f(0.2f, 0.3f);
     glVertex2f(0.2f,  0.2f);
     glVertex2f(-0.6f,  0.2f);
-
+//================================================
     glColor3f(1.0f, 0.0f, 0.0f);
     glVertex2f(-0.6f, 0.2f);
     glVertex2f(0.2f, 0.2f);
     glVertex2f(0.2f,  0.1f);
     glVertex2f(-0.6f,  0.1f);
-
+//================================================
     glColor3f(0.50f, 0.0f, 0.50f);
     glVertex2f(-0.6f, 0.1f);
     glVertex2f(0.2f, 0.1f);
     glVertex2f(0.2f,  0.0f);
     glVertex2f(-0.6f,  0.0f);
-
+//================================================
     glColor3f(0.50f, 0.50f, 0.50f);
     glVertex2f(-0.6f, 0.f);
     glVertex2f(0.2f, 0.f);
     glVertex2f(0.2f,  -0.1f);
     glVertex2f(-0.6f,  -0.1f);
-
+//================================================
     glColor3f(0.50f, 0.50f, 0.10f);
     glVertex2f(-0.6f, -0.1f);
     glVertex2f(0.2f, -0.1f);
     glVertex2f(0.2f,  -0.2f);
     glVertex2f(-0.6f,  -0.2f);
-
+//================================================
     // flag strict
     glColor3f(0.0f, 0.0f, 0.60f);
-    glVertex2f(-0.6f, 0.5f);
-    glVertex2f(-0.55f, 0.5f);
+    glVertex2f(-0.6f, 0.6f);
+    glVertex2f(-0.55f, 0.6f);
     glVertex2f(-0.55f,  -0.7f);
     glVertex2f(-0.6f,  -0.7f);
+//###################################################
 
+    glBegin(GL_QUADS);
+	glColor3f(1.0f, 1.0f, 1.0f);
+	glVertex2f(-0.60f, -0.7f);
+	glVertex2f(-0.60f, -0.8f);
+	glVertex2f(-0.50f, -0.8f);
+	glVertex2f(-0.50f, -0.7f);
+//================================================
     glEnd();
 
 	glFlush();  // Render now
